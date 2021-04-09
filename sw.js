957ed865301e1688dca88b8000161f89380f7a90
kzlamaniec <47745197+kzlamaniec@users.noreply.github.com>
2021-04-09 21:06:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 //}
 if ('serviceWorker' in navigator) {
   // declaring scope manually
-  navigator.serviceWorker.register('/sw.js', {scope: '/index.html/'}).then(function(registration) {
+  navigator.serviceWorker.register('/sw.js', {scope: '/index.html'}).then(function(registration) {
     console.log('Service worker registration succeeded:', registration);
   }, /*catch*/ function(error) {
     console.log('Service worker registration failed:', error);
