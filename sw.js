5b5df2bfcfd2e347396f346ae6b6b95a96261854
afiszer <66952919+afiszer@users.noreply.github.com>
2021-04-09 20:58:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
-if ('serviceWorker' in navigator) {
+//if ('serviceWorker' in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+ // navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    //console.log('Service worker registration succeeded:', registration);
+ // }, /*catch*/ function(error) {
+    //console.log('Service worker registration failed:', error);
+  //});
+//} else {
+  //console.log('Service workers are not supported.');
+//}
+if ('serviceWorker' in navigator) {
+  // declaring scope manually
+  navigator.serviceWorker.register('/sw.js', {scope: '/product/'}).then(function(registration) {
     console.log('Service worker registration succeeded:', registration);
   }, /*catch*/ function(error) {
     console.log('Service worker registration failed:', error);
